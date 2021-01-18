ab2a39fad8afe95e948bf9dd1051620e21fc278b
fengpei0114 <feng_pei0114@163.com>
2021-01-18 22:40:42 +0800

    first commit

--- homework_two.sh
+++ homework_two.sh
@@ -1,10 +1,12 @@
 #！ /bin/bash
 basepath=$(cd `dirname $0`; pwd)
-Msg = "$1"
-echo "Msg"
-echo "====================="
+
+commitMsg="first commit"
+if [ $1 ];then commitMsg="$1" 
+fi
+
 git init
 git add .
-git commit -m "first commit"
+git commit -m "$commitMsg"
 git remote add origin git@github.com:fengpei0114/cli_test.git
 git push -u origin master
